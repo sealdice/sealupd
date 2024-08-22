1836f418d1afc19c1ed378e9aa34a3a936970dcf
檀轶步棋 <57583509+oissevalt@users.noreply.github.com>
2024-08-22 12:25:54 +0800

    imp: Narrow refresh kind

--- src/proc.rs
+++ src/proc.rs
@@ -3,7 +3,22 @@
 use std::{process, thread, time::Duration};
 
 use log::debug;
-use sysinfo::{Pid, ProcessRefreshKind, RefreshKind, System};
+use sysinfo::{Pid, ProcessRefreshKind, ProcessesToUpdate, RefreshKind, System};
+
+struct RefreshSpecs {
+    refresh_kind: RefreshKind,
+    proc_refresh_kind: ProcessRefreshKind,
+}
+
+impl RefreshSpecs {
+    pub fn new() -> Self {
+        let proc_refresh_kind = ProcessRefreshKind::new();
+        RefreshSpecs {
+            refresh_kind: RefreshKind::new().with_processes(proc_refresh_kind),
+            proc_refresh_kind,
+        }
+    }
+}
 
 /// Waits for the process with specified PID to finish. If the program is spawned
 /// by that process, finishes waiting if the program has inherited the PID.
@@ -14,21 +29,24 @@ pub fn wait(pid: u32) {
         return;
     }
 
-    let mut sys = System::new_with_specifics(
-        RefreshKind::new().with_processes(ProcessRefreshKind::everything()),
-    );
+    let specs = RefreshSpecs::new();
+    let mut sys = System::new_with_specifics(specs.refresh_kind);
 
     loop {
         match sys.process(pid) {
             None => break,
             Some(proc) => {
                 let pname = proc.name();
-                debug!("Found process {} \"{}\"", pid, pname.escape_debug());
+                debug!(
+                    "Found process {} \"{}\"",
+                    pid,
+                    pname.to_string_lossy().escape_debug()
+                );
                 if pname == env!("CARGO_PKG_NAME") || prog_pid == pid {
                     break;
                 }
 
-                sys.refresh_processes();
+                sys.refresh_processes_specifics(ProcessesToUpdate::All, specs.proc_refresh_kind);
                 prog_pid = Pid::from_u32(process::id());
                 thread::sleep(Duration::from_secs(1));
             }
